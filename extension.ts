0fd4d0a2c21ea9fc13c23a2d61eb52125f6945ea
Paul Clavier <paul@clavier.uk>
2019-07-03 21:30:18 +0100

    changes to blocks

--- extension.ts
+++ extension.ts
@@ -12,7 +12,7 @@ namespace barometer {
 
     /**
      * Changes the default I2C address from 0x77 to the provided address
-     * @param The I2C address to use
+     * @param The I2C address to use; eg: 0x76
      */
     //% block
     //% shim=bmp280::setAddress
@@ -21,14 +21,14 @@ namespace barometer {
     /**
      * Returns the pressure read by the BMP280's barometer in hPa
      */
-    //% block
+    //% block="pressure"
     //% shim=bmp280::getPressure
     export function getPressure(): number { return 0 }
 
     /**
      * Returns the temperature read by the BMP280's temperature sensor in Celcius
      */
-    //% block
+    //% block="temperature"
     //% shim=bmp280::getTemperature
     export function getTemperature(): number { return 0 }
 }
