418e90df512a4037ac6b63bccafa606f72a66b55
Paul Clavier <paul@clavier.uk>
2019-07-18 12:31:18 +0100

    Wrote test suite

--- test.ts
+++ test.ts
@@ -1,1 +1,22 @@
-// tests go here; this will not be compiled when this package is used as a library
+function getReadings(): {pressure: number, temp: number} {
+    return {
+        pressure: barometer.getPressure(),
+        temp: barometer.getTemperature()
+    }
+}
+
+// Test that the barometer returns nothing with an invalid address
+barometer.setAddress(0x00);
+barometer.init();
+
+let readings = getReadings();
+control.assert(readings.pressure === 0);
+control.assert(readings.temp === 0);
+
+// Test that the barometer returns readings with the correct address
+barometer.setAddress(0x77);
+barometer.init();
+
+readings = getReadings();
+control.assert(readings.pressure !== 0);
+control.assert(readings.temp !== 0);
